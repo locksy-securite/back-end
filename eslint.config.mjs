e6bf78cac5292abe27f29c94cdc78c838d6ccdd6
Kavé <kave.t@MacBook-Pro-de-VENTE.local>
2025-11-28 17:00:46 +0100

    Fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,29 @@
-import ts from "@typescript-eslint/eslint-plugin";
-import { defineConfig } from "eslint/config";
+// eslint.config.mjs
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
 
-export default defineConfig([
-
-  { ignores: ["dist/*/"] },
-
-
+export default [
   {
-    files: [".ts"],
-    parser: "@typescript-eslint/parser",
-    parserOptions: { project: "./tsconfig.json" },
-    plugins: { "@typescript-eslint": ts },
-    extends: ["plugin:@typescript-eslint/recommended"],
-    files: ["**/*.{ts,tsx}"],
-    ignores: ["**/*.spec.ts"], // exclut les tests
+    files: ["src/**/*.ts", "src/**/*.tsx", "src/**/*.js"],
+    languageOptions: {
+      parser: tsParser, // objet importé
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
     rules: {
-    // tes règles ici
-  },
-  },
-
-  
-
-
-  {
-    files: [".spec.ts"],
-    rules: {
-      "@typescript-eslint/no-unused-vars": "off", 
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "no-console": "warn",
+      "@typescript-eslint/no-empty-function": "warn",
+      "@typescript-eslint/adjacent-overload-signatures": "error",
+      "@typescript-eslint/array-type": ["error", { default: "array-simple" }],
+      "@typescript-eslint/ban-ts-comment": "error",
+      "@typescript-eslint/no-inferrable-types": "off",
     },
   },
-]); 
+];
