27b23c816afa929e2919fe4f16dd2bd4d4fd9bc2
LLAI21 <lucas.travail21@gmail.com>
2025-12-01 11:34:39 +0100

    add app.module.ts and add app.service.ts

---
+++ src/users/user.model.ts
@@ -0,0 +1,17 @@
+// src/users/user.entity.ts
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string; // sera haché
+
+  @Column()
+  creditCard: string; // ⚠️ à chiffrer en vrai projet
+}
