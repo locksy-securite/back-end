d86a9193f9c51c258e4e12aaf8c3371baab40e61
LLAI21 <lucas.travail21@gmail.com>
2025-12-01 14:42:05 +0100

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/user.module';
+//import { DatabaseModule } from './database/database.module';
+//import { User } from './users/user.entity';
+import { DatabaseModule } from "./database/database.module";
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from "./users/user.model";
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PGHOST,
+      port: parseInt(process.env.PGPORT ?? '5432', 10),
+      username: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
+      autoLoadEntities: true, // charge toutes les entités automatiquement
+      synchronize: true,      // ⚠️ pratique en dev, à éviter en prod
+    }), UsersModule],
+  exports: [],
 })
 export class AppModule {}
