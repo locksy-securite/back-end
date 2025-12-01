0244e329cdc2731ddfb2e546794072d01fbd5645
LLAI21 <lucas.travail21@gmail.com>
2025-12-01 14:44:19 +0100

    update database.module.ts

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
-import { DatabaseService } from './database.service';
+import { databaseProviders } from './database.providers';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
+import { Password } from './entity/password.entity';
+import { Note } from './entity/note.entity';
 
 @Module({
-  providers: [DatabaseService],
-  exports: [DatabaseService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PGHOST || 'localhost',
+      port: parseInt(process.env.PGPORT || '5432', 10),
+      username: process.env.PGUSER || 'postgres',
+      password: process.env.PGPASSWORD || 'postgres',
+      database: process.env.PGDATABASE || 'test',
+      entities: [User, Password, Note], // tu peux ajouter CreditCard si besoin
+      synchronize: false, // ⚠️ false en prod, true seulement en dev
+    }),
+  ],
+  providers: [...databaseProviders],
+  exports: [...databaseProviders],
 })
 export class DatabaseModule {}
