77c41d9c6614dfd56a3a3cbf8300bb0f2167e96f
sowla <sowla@example.com>
2025-12-02 23:12:30 +0100

    Integrate TypeORM in users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { JwtAuthGuard } from 'src/auth/jwt.guard';
 import { DatabaseModule } from '../database/database.module';
+import { User } from '../database/entity/user.entity';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, TypeOrmModule.forFeature([User])],
   providers: [UsersService, JwtAuthGuard],
   controllers: [UsersController],
   exports: [UsersService],
