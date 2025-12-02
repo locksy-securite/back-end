114a843909bf7e4bd75dcbcbc83b9969edb4c2dc
c-dal <chloe.dallais@icloud.com>
2025-12-02 18:25:13 +0100

    Corrections pour GCP (#16)

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.NEST_PORT || 3000);
-  console.log('Serveur NestJS démarré sur le port 3000');
+  const port = process.env.PORT || process.env.NEST_PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Serveur NestJS démarré sur le port ${port}`);
 }
 bootstrap();
