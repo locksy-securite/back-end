a44da951d391faad271b9082dd702609a3855a79
c-dal <chloe.dallais@icloud.com>
2025-12-05 15:06:35 +0100

    Dev 2 (#31)
    * Add allowed origin to .env variables
    
    * Correct ssl value
    
    * Correct port import
    
    * Correct unused variable and import
    
    * Corrected ssl requirement
    
    * Replace configService with process.env
    
    * Add endpoint prefix to fit front
    
    * Remove global prefix setting for API
    
    * Ran audit command
    
    * Simplify CORS configuration in main.ts
    
    * Add OPTIONS method to CORS configuration
    
    * Refactor CORS settings for dynamic origin handling
    
    Updated CORS configuration to allow dynamic origin validation.
    
    * Remove global prefix setting for API
    
    * Change app.listen to bind to 0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
   });
 
   const port = process.env.PORT || process.env.NEST_PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Serveur NestJS démarré sur le port ${port}`);
 }
 bootstrap();
